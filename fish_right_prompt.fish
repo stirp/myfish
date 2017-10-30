526a28c5b13c25abafa3d85289c156ec860e8568
王小冋 <heroicstirp@163.com>
2017-10-30 16:01:28 +0800

    udpate

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,6 +2,6 @@ function fish_right_prompt
   set -l st $status
 
   if [ $st != 0 ];
-    echo (set_color red) ↵ $st(set_color normal)
+    echo (set_color red) date "+%H:%M:%S" ↵ $st(set_color normal)
   end
 end
